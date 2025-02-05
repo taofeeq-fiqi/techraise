89743bb11c8acbb425ce203c5eaf119e9818e0ea
taofeeq-fiqi <taofeeqallihakeem01@gmail.com>
2025-02-05 11:02:57 +0100

    Add files via upload

---
+++ resources/views/users/passwordreset.blade.php
@@ -0,0 +1,38 @@
+<x-layout>
+    <x-card class="p-10 max-w-lg mx-auto mt-24">
+                    <header class="text-center">
+                        <h2 class="text-2xl font-bold uppercase mb-1">
+                            Password reset
+                        </h2>
+                        <p class="mb-4">Provide registered Email for password link</p>
+                    </header>
+
+                    <form method="POST" action="">
+                        @csrf
+                      
+
+                        <div class="mb-6">
+                            <label for="email" class="inline-block text-lg mb-2"
+                                >Email</label
+                            >
+                            <input
+                                type="email"
+                                class="border border-gray-200 rounded p-2 w-full"
+                                name="email" value="{{old('email')}}"
+                            />
+                            @error('email')
+                            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                            @enderror
+                        </div>
+                        <div class="mb-6">
+                            <button
+                                type="submit"
+                                class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
+                            >
+                                reset password
+                            </button>  
+                        </div>
+                    </form>
+                </div>
+    </x-card>
+</x-layout>
